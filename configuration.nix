e530d44474177cac54bd7489bbba88cf139f9b43
Rajwol Chapagain <rajwolchapagain@gmail.com>
2024-03-04 13:38:15 -0500

    Add kdeconnect

--- configuration.nix
+++ configuration.nix
@@ -51,8 +51,8 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-  services.xserver.displayManager.defaultSession = "plasmawayland";
+  services.xserver.desktopManager.plasma6.enable = true;
+  services.xserver.displayManager.defaultSession = "plasma";
   programs.dconf.enable = true;
 
   # Configure keymap in X11
@@ -201,6 +201,7 @@
       };
   };
 
+  programs.kdeconnect.enable = true;
   virtualisation.docker.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
