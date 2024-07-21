006c9978a7da3fabf6e352e65fad2b43f09588c9
Rajwol Chapagain <rajwolchapagain@gmail.com>
2024-07-21 09:33:14 -0400

    Add cuda cache

--- configuration.nix
+++ configuration.nix
@@ -176,4 +176,13 @@
   system.stateVersion = "23.11"; # Did you read the comment?
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nix.settings = {
+      substituters = [
+	"https://cuda-maintainers.cachix.org"
+      ];
+      trusted-public-keys = [
+	"cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      ];
+    };
 }
