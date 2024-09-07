0dad8009b3dc6b8d9f27c57fdd2be903ad2785a4
Rajwol Chapagain <rajwolchapagain@gmail.com>
2024-09-07 01:26:35 -0400

    Add crude way to sync pws through drive

--- configuration.nix
+++ configuration.nix
@@ -128,6 +128,8 @@
 	clean = "sudo nix-collect-garbage";
 	wipe = "sudo nix-collect-garbage --delete-old";
 	ls = "eza --icons";
+	push_pw = "rclone copy ~/Documents/Passwords.kdbx remote:";
+	pull_pw = "rclone copy remote:Passwords.kdbx ~/Documents/";
     };
 
     promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
