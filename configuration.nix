41add7291e2395f685543ed5898f7a2baf779ddd
Rajwol Chapagain <rajwolchapagain@gmail.com>
2024-09-11 16:04:55 -0400

    Add steam

--- configuration.nix
+++ configuration.nix
@@ -144,6 +144,7 @@
   };
   
   programs.adb.enable = true;
+  programs.steam.enable = true;
   
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
