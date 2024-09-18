d9b3998c1d290161806ff166916765d17a7e6b89
Rajwol Chapagain <rajwolchapagain@gmail.com>
2024-09-18 13:03:13 -0400

    Enable docker

--- configuration.nix
+++ configuration.nix
@@ -145,6 +145,8 @@
   
   programs.adb.enable = true;
   programs.steam.enable = true;
+
+  virtualisation.docker.enable = true;
   
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
