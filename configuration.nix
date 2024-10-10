20d63848662ccdda71c1beeaa887e0eb1a510544
Rajwol Chapagain <rajwolchapagain@gmail.com>
2024-10-10 15:37:19 -0400

    Remove unused apps. Fix indentation

--- configuration.nix
+++ configuration.nix
@@ -1,192 +1,138 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./systemPackages.nix
-      ./nvidia.nix
+    imports = [       
+        ./hardware-configuration.nix
+        ./systemPackages.nix
+        ./nvidia.nix
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 5;
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.systemd-boot.configurationLimit = 5;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networking.hostName = "nixos"; 
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Enable networking
+    networking.networkmanager.enable = true;
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+    hardware.bluetooth.enable = true;
+    hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+    time.timeZone = "America/New_York";
 
-  # Set your time zone.
-  time.timeZone = "America/New_York";
+    i18n.defaultLocale = "en_US.UTF-8";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "en_US.UTF-8";
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
+    };
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+    # Enable the KDE Plasma Desktop Environment.
+    services.displayManager.sddm.enable = true;
+    services.desktopManager.plasma6.enable = true;
+    services.displayManager.defaultSession = "plasma";
+    programs.dconf.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.displayManager.defaultSession = "plasma";
-  programs.dconf.enable = true;
+    # Configure keymap in X11
+    services.xserver = {
+        xkb.layout = "us";
+        xkb.variant = "";
+    };
 
-  # Configure keymap in X11
-  services.xserver = {
-    xkb.layout = "us";
-    xkb.variant = "";
-  };
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+    # Enable sound with pipewire.
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+    };
 
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    users.defaultUserShell = pkgs.zsh;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+    users.users.rajwol = {
+        isNormalUser = true;
+        description = "Rajwol Chapagain";
+        extraGroups = [ "networkmanager" "wheel" ];
+        useDefaultShell = true;
+        packages = with pkgs; [
+        ];
+    };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  users.defaultUserShell = pkgs.zsh;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.rajwol = {
-    isNormalUser = true;
-    description = "Rajwol Chapagain";
-    extraGroups = [ "networkmanager" "wheel" "adbusers"];
-    useDefaultShell = true;
-    packages = with pkgs; [
+    fonts.packages = with pkgs; [
+        fira
+        roboto
+        nerdfonts
     ];
-  };
-  
-  fonts.packages = with pkgs; [
-    fira
-    roboto
-    nerdfonts
-  ];
 
-  environment.sessionVariables = {
-    MOZ_USE_XINPUT2 = "1";
-    EDITOR = "nvim";
-  };
-
-  programs.firefox = {
-      enable = true;
-      preferences = {
-        "widget.use-xdg-desktop-portal.file-picker" = 1;
-	"media.hardware-video-decoding.force-enabled" = true;
-      };
-  };
-
-  programs.zsh = {
-    enable = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    enableCompletion = true;
-    shellAliases = {
-	editconfig = "sudo nvim /home/rajwol/.dotfiles/configuration.nix";
-	rebuild = "sudo nixos-rebuild switch --flake /home/rajwol/.dotfiles/";
-	update = "sudo nix flake update /home/rajwol/.dotfiles/";
-	clean = "sudo nix-collect-garbage";
-	wipe = "sudo nix-collect-garbage --delete-old";
-	ls = "eza --icons";
-	push_pw = "rclone copy ~/Documents/Passwords.kdbx remote:Personal/";
-	pull_pw = "rclone copy remote:Personal/Passwords.kdbx ~/Documents/";
+    environment.sessionVariables = {
+        MOZ_USE_XINPUT2 = "1";
+        EDITOR = "nvim";
     };
 
-    promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-    ohMyZsh = {
-	enable = true;
+
+    #=============== Programs enabled via options ===============
+
+    programs.firefox = {
+        enable = true;
+        preferences = {
+            "widget.use-xdg-desktop-portal.file-picker" = 1;
+            "media.hardware-video-decoding.force-enabled" = true;
+        };
     };
-  };
 
-  programs.nh = {
-    enable = true;
-    flake = "/home/rajwol/.dotfiles";
-  };
-  
-  programs.adb.enable = true;
+    programs.zsh = {
+        enable = true;
+        autosuggestions.enable = true;
+        syntaxHighlighting.enable = true;
+        enableCompletion = true;
+        shellAliases = {
+            editconfig = "sudo nvim /home/rajwol/.dotfiles/configuration.nix";
+            rebuild = "sudo nixos-rebuild switch --flake /home/rajwol/.dotfiles/";
+            update = "sudo nix flake update /home/rajwol/.dotfiles/";
+            clean = "sudo nix-collect-garbage";
+            wipe = "sudo nix-collect-garbage --delete-old";
+            ls = "eza --icons";
+            push_pw = "rclone copy ~/Documents/Passwords.kdbx remote:Personal/";
+            pull_pw = "rclone copy remote:Personal/Passwords.kdbx ~/Documents/";
+        };
 
-  virtualisation.podman = {
-    enable = true;
-    dockerCompat = true;
-  };
-
-  services.thermald.enable = true;
- 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/:options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  nix.settings = {
-      substituters = [
-	"https://cuda-maintainers.cachix.org"
-      ];
-      trusted-public-keys = [
-	"cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      ];
+        promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        ohMyZsh = {
+            enable = true;
+        };
     };
+
+    programs.nh = {
+        enable = true;
+        flake = "/home/rajwol/.dotfiles";
+    };
+
+    programs.steam = {
+        enable = true;
+    };
+
+    #=============== Services ===============
+
+    services.thermald.enable = true;
+
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    
+    system.stateVersion = "23.11"; # Don't change
 }
