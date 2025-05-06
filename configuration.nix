ff2a331844c65448770b2a90b6fcda839d1b5df3
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-05-06 17:37:37 -0400

    Create minimal initial configuration

---
+++ configuration.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+      ./system-packages.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/New_York";
+
+  users.users.rajwol = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
+
+  system.stateVersion = "24.11"; # DO NOT CHANGE
+}
+
