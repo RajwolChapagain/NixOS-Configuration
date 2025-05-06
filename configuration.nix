f69c4408e7b5832de086cf0d71efbdbe7139c710
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-05-06 19:15:26 -0400

    Enable sound

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,15 @@
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
   users.users.rajwol = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
