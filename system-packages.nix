a121a716d823596fbed53185e6ac3d2ee629f1a6
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-05-07 14:52:47 -0400

    Fix Firefox file picker

--- system-packages.nix
+++ system-packages.nix
@@ -5,11 +5,18 @@
 
   programs.steam.enable = true;
 
+  programs.firefox = {
+    enable = true;
+    preferences = {
+      "widget.use-xdg-desktop-portal.file-picker" = 1;
+      "media.hardware-video-decoding.force-enabled" = true;
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     neovim
     git
     kitty
-    firefox
     thunderbird
     keepassxc
   ];
