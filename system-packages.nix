d12c52ea3db5354922dc16105462b5e6e5b4a782
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-05-07 14:49:02 -0400

    Enable Steam

--- system-packages.nix
+++ system-packages.nix
@@ -2,12 +2,15 @@
 
 {
   nixpkgs.config.allowUnfree = true;
-  
+
+  programs.steam.enable = true;
+
   environment.systemPackages = with pkgs; [
     neovim
     git
     kitty
     firefox
     thunderbird
+    keepassxc
   ];
 }
