f599ea40eeb7c3bab795f0af98db24073079c736
Rajwol Chapagain <rajwolchapagain@gmail.com>
2026-02-01 21:24:28 -0500

    Enable dns for podman

--- system-packages.nix
+++ system-packages.nix
@@ -113,6 +113,7 @@
         podman = {
             enable = true;
             dockerCompat = true;
+            defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
         };
 	};
 
