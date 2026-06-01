a88279e1cc1a061f9f42cb93574ba41cdb83ddee
Rajwol Chapagain <rajwolchapagain@gmail.com>
2026-06-01 19:29:46 -0400

    Update flake

--- configuration.nix
+++ configuration.nix
@@ -10,8 +10,6 @@
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
 
-    boot.loader.systemd-boot.extraEntries."ubuntu.conf" = '' title Ubuntu efi /EFI/ubuntu/shimx64.efi '';
-
 	networking.hostName = "nixos";
 	networking.networkmanager.enable = true;
 
